fcd4aae31ccf2348fbf3acdb77891e47fc038450
dan0m1 <bohdan.shevchenko182@gmail.com>
2024-07-05 22:24:13 +0300

    minor updates

--- src/v1/api/controllers/TaskController.ts
+++ src/v1/api/controllers/TaskController.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import {CreateTasksDTO} from "../../DTOs/CreateTasksDTO";
 import {TaskService} from "../services/TaskService";
 import { DeleteTaskDTO } from '../../DTOs/DeleteTaskDTO';
+import { TaskResponse } from '../../responses/TaskResponse';
 
 @Controller("task")
 export class TaskController {
@@ -9,13 +10,12 @@ export class TaskController {
     }
 
     @Post()
-    async create(@Body() body: CreateTasksDTO){
-        const task = await this.taskService.create(body);
-        return task;
+    async create(@Body() body: CreateTasksDTO): Promise<TaskResponse>{
+        return await this.taskService.create(body);
     }
 
     @Delete()
-    async delete(@Body() body: DeleteTaskDTO){
+    async delete(@Body() body: DeleteTaskDTO): Promise<boolean>{
         return await this.taskService.delete(body)
     }
 
--- src/v1/database/entities/DbTask.ts
+++ src/v1/database/entities/DbTask.ts
@@ -1,5 +1,3 @@
-import {User} from "@prisma/client";
-
 export class DbTask {
     id: number;
     description: string;
